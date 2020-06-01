29b4f2d69a4b0cd185037e5df283dc6b3fc57f79
Sivadej K <bombyk@gmail.com>
2020-05-31 21:03:08 -0800

    recursion exercises completed

--- dsa-recursion/recursion.js
+++ dsa-recursion/recursion.js
@@ -10,14 +10,12 @@ function count(n=1) {
 
 function sum(nums) {
   if(nums.length === 0) return 0;
-
   return nums[0] + sum(nums.slice(1));
 }
 
 function product(nums) {
   // base case: when array is empty
   if (nums.length === 0) return 1;
-
   return nums[0] * product(nums.slice(1));
 }
 
@@ -32,7 +30,6 @@ function product(nums) {
 
 function longest(words, max=0) {
   if (words.length === 0) return max;
-
   if (words[0].length > max) max = words[0].length;
   return longest(words.slice(1), max);
 }
@@ -45,7 +42,6 @@ function longest(words, max=0) {
 
 function everyOther(str) {
   if (str.length === 0) return '';
-
   return str[0] + everyOther(str.slice(2));
 }
 //solution: return only odd index values? accumulate as string
@@ -55,7 +51,6 @@ function everyOther(str) {
 
 function isPalindrome(str) {
   if (str.length === 0) return true;
-
   if (str[0] === str[str.length-1]) return isPalindrome(str.slice(1,str.length-1));
   return false;
 }
@@ -65,28 +60,52 @@ function isPalindrome(str) {
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
-
+function findIndex(arr, val, i=0) {
+  if (arr.length === i) return -1;
+  if (arr[i] === val) return i;
+  return findIndex(arr, val, i+1);
 }
 // findIndex( ['cat','dog','horse'], 'dog' ) ==> 1
 
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str) {
-
+  if (str === '') return '';
+  return Array.from(str).pop() + revString(str.slice(0,-1));
 }
+// pop last char of string and return with recursive revString function.
+// base case empty string
+
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
-
+const gatherStrings = (obj, out=[], i=0) => {
+  const vals = Object.values(obj)
+  if (vals.length === i) return out;
+  if (typeof(vals[i]) === 'string') out.push(vals[i]);
+  if (typeof(vals[i]) === 'object') out.push(...gatherStrings(vals[i]));
+  return gatherStrings(obj, out, i+1);
 }
+// iterate over values to determine type. include typeof string in array. recurse func if typeof 'object'
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {
+function binarySearch(arr, val, start=0, end=arr.length-1) {
+  // base case
+  if (start > end) return -1;
 
+  // find mid
+  let mid = Math.floor((start+end)/2);
+
+  // compare mid to target value
+  if (arr[mid] === val) return mid;
+
+  // if mid is greater than target value, search lower half
+  if (arr[mid] > val) return binarySearch(arr, val, start, mid-1);
+
+  // else search upper half
+  if (arr[mid] < val) return binarySearch(arr, val, mid+1, end);
 }
 
 module.exports = {
