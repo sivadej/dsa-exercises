2377d2e1e4ce14321f94c3c388cd28da4c367562
Sivadej K <bombyk@gmail.com>
2020-06-01 22:37:16 -0800

    binary tree exercise

--- dsa-trees/binary-tree.js
+++ dsa-trees/binary-tree.js
@@ -17,28 +17,66 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    if (!this.root) return 0;
 
+    function getMinDepth(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return getMinDepth(node.right) + 1;
+      if (node.right === null) return getMinDepth(node.left) + 1;
+      return Math.min(getMinDepth(node.left), getMinDepth(node.right)) + 1;
+    }
+    return getMinDepth(this.root);
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
-
+    if (!this.root) return 0;
+    function getMaxDepth(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return getMaxDepth(node.right) + 1;
+      if (node.right === null) return getMaxDepth(node.left) + 1;
+      return Math.max(getMaxDepth(node.left), getMaxDepth(node.right)) + 1;
+    }
+    return getMaxDepth(this.root);
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
-
+    let result = 0;
+    function getMaxSum(node) {
+      if (node === null) return 0;
+      const leftSum = getMaxSum(node.left);
+      const rightSum = getMaxSum(node.right);
+      result = Math.max(result, node.val + leftSum + rightSum);
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+    }
+    getMaxSum(this.root);
+    return result;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if (!this.root) return null;
+    let queue = [this.root];
+    let closest = null;
 
+    while(queue.length) {
+      let currentNode = queue.shift();
+      let currentVal = currentNode.val;
+      let higherThanLowerBound = currentVal > lowerBound;
+      let shouldReassignClosest = currentVal < closest || closest === null;
+
+      if (higherThanLowerBound && shouldReassignClosest) closest = currentVal;
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+    return closest;
   }
 
   /** Further study!
