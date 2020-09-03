3beabb791e88131a1096c6602b624471334fcb6d
Sivadej K <sbkitchpanich@gmail.com>
2020-09-02 21:40:05 -0800

    Update graph.js

--- dsa-graphs/graph.js
+++ dsa-graphs/graph.js
@@ -10,26 +10,149 @@ class Graph {
     this.nodes = new Set();
   }
 
-  // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
-  // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    // Add an array of verticies to our graph
 
-  // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
-  // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    // Connect two verticies
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
-  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
-  // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  removeVertex(vertex) {
+    for (let node of this.nodes) {
+      if (node.adjacent.has(vertex)) {
+        node.adjacent.delete(vertex);
+      }
+    }
+    this.nodes.delete(vertex);
+  }
 
-  // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+
+    function traverse(vertex) {
+      // base case
+      if (!vertex) {
+        return null;
+      }
+      // visit node
+      visited.add(vertex);
+      result.push(vertex.value);
+
+      // visit neighbors
+      vertex.adjacent.forEach(neighbor => {
+        if (!visited.has(neighbor)) {
+          return traverse(neighbor);
+        }
+      });
+    }
+
+    traverse(start);
+
+    return result;
+  }
+
+  depthFirstSearchIterative(start) {
+    // Create an empty stack
+    const stack = [start];
+    const result = [];
+    const visited = new Set();
+    let currentVertex;
+
+    // visit node
+    visited.add(start);
+
+    // while there are still neighbors to visit
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex.value);
+
+      // visit neighbors and push onto stack
+      currentVertex.adjacent.forEach(neighbor => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  breadthFirstSearch(start) {
+    // Create an empty queue
+    const queue = [start];
+    const result = [];
+    const visited = new Set();
+    let currentVertex;
+
+    // visit node
+    visited.add(start);
+
+    // While there is still remaining vertices in queue
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex.value);
+
+      // visit neighbors
+      currentVertex.adjacent.forEach(neighbor => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  shortestPath(start, end) {
+    if (start === end) {
+      return [start.value];
+    }
+
+    var queue = [start];
+    let visited = new Set();
+    let predecessors = {};
+    let path = [];
+
+    while (queue.length) {
+      let currentVertex = queue.shift();
+
+      if (currentVertex === end) {
+        let stop = predecessors[end.value];
+        while (stop) {
+          path.push(stop);
+          stop = predecessors[stop];
+        }
+        path.unshift(start.value);
+        path.reverse();
+        return path;
+      }
+
+      visited.add(currentVertex);
+      for (let vertex of currentVertex.adjacent) {
+        if (!visited.has(vertex)) {
+          predecessors[vertex.value] = currentVertex.value;
+          queue.push(vertex);
+        }
+      }
+    }
+  }
 }
 
-module.exports = {Graph, Node}
+module.exports = { Graph, Node };
